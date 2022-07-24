1adb5ccd31979cb9161c5647576f9b814e7a4147
Mario Mainardes <mainardes.m@gmail.com>
2022-07-24 15:20:12 -0300

    Projeto Criado

---
+++ src/main/java/com/nelioalves/msnelio/MsnelioApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.msnelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsnelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/msnelio/MsnelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.msnelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
